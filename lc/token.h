c05351b331d960135df577898eca2f450d088bf9
Luigi D. C. Soares <luigi.soares@sga.pucminas.br>
2019-02-25 21:20:36 -0300

    Initial tokens

---
+++ lc/token.h
@@ -0,0 +1,16 @@
+#ifndef LC_TOKEN_H_
+#define LC_TOKEN_H_
+
+namespace lc::core {
+
+    enum class Token {
+        IfKw,
+        ElseKw,
+        ForKw,
+        AssignKw
+    };
+
+}
+
+#endif
+
